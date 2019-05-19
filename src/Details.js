1e009f57a428f764344f81afd16fef73c722ae1f
Tomi Adebanjo <tomiadebanjo@gmail.com>
2019-05-20 00:45:57 +0100

    create modal component

--- src/Details.js
+++ src/Details.js
@@ -2,13 +2,17 @@ import React from "react";
 import pf from "petfinder-client";
 import { navigate } from "@reach/router";
 import Carousel from "./Carousel";
+import Modal from "./Modal";
 
 const petfinder = pf({});
 
 class Details extends React.Component {
   state = {
-    loading: true
+    loading: true,
+    showModal: true
   };
+
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
 
   componentDidMount() {
     petfinder.pet
@@ -46,8 +50,14 @@ class Details extends React.Component {
       return <h1>Loading...</h1>;
     }
 
-    const { animal, breed, location, description, media } = this.state;
-
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      media,
+      showModal
+    } = this.state;
     return (
       <div className="details">
         <Carousel media={media} />
@@ -56,7 +66,17 @@ class Details extends React.Component {
           <h2>
             {animal} - {breed} - {location}
           </h2>
+          <button onClick={this.toggleModal}>Adopt {name}</button>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <h1>Would you like to adopt {name}?</h1>
+              <div className="buttons">
+                <button onClick={this.toggleModal}>Yes</button>
+                <button onClick={this.toggleModal}>Definitely Yes</button>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
